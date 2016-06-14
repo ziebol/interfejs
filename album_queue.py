ebde1968d8c6b328935836fc0c4dffa3773352b9
EC2 Default User <ec2-user@ip-172-31-16-20.eu-central-1.compute.internal>
2016-06-14 11:48:43 +0000

    Poprawki w UI

--- album_queue.py
+++ album_queue.py
@@ -1,6 +1,9 @@
 import boto3
-import json sqs = boto3.resource('sqs')
+import json
+
+sqs = boto3.resource('sqs')
 albums = sqs.get_queue_by_name(QueueName='zieba-album')
+
 def request_album(data):
   dataAsString = json.dumps(data)
   response = albums.send_message(MessageBody=dataAsString)
